45f4f79a564b359563e5e9f6401ebfa9d9f1234e
Ji Han Lim <jhlim101@snu.ac.kr>
2024-03-20 03:34:25 +0900

    feat: added ReturnQueryTypeVisitor class

--- run.py
+++ run.py
@@ -1,15 +1,31 @@
-from lark import Lark, Token
+# for testing purposes:
+# select ID from students;
+# create table test(id int, primary key(id));
+
+from lark import Lark, Visitor
 
 PROMPT = 'DB_2022-13616>'
 
 with open('grammar.lark') as file:
     sql_parser = Lark(file.read(), start='command', lexer='basic')
 
+class ReturnQueryTypeVisitor(Visitor):
+    def select_query(self, tree):
+        assert tree.data == 'select_query'
+        print(PROMPT + '\'SELECT\' requested')
+    
+    def create_table_query(self, tree):
+        assert tree.data == 'create_table_query'
+        print(PROMPT + '\'CREATE TABLE\' requested')
+
+    def delete_query(self, tree):
+        assert tree.data == 'delete_query'
+        print(PROMPT + '\'DELETE\' requested')
+
 if __name__ == "__main__":
     bool_exit = False
     while not bool_exit:
         query = input(PROMPT)
-        print('-----Parsed Result-----')
         output_tree = sql_parser.parse(query)
-        print(output_tree.pretty())
+        ReturnQueryTypeVisitor().visit(output_tree)
         
