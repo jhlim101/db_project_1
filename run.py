c59c4813436058151d43900567d502e5d95b3f69
Ji Han Lim <jhlim101@snu.ac.kr>
2024-03-20 02:53:40 +0900

    feat: added PROMPT variable, created main while loop

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-from lark import Lark
+from lark import Lark, Token
+
+PROMPT = 'DB_2022-13616>'
 
 with open('grammar.lark') as file:
     sql_parser = Lark(file.read(), start='command', lexer='basic')
+
+if __name__ == "__main__":
+    bool_exit = False
+    while not bool_exit:
+        query = input(PROMPT)
+        print('-----Parsed Result-----')
+        output_tree = sql_parser.parse(query)
+        print(output_tree.pretty())
+        
