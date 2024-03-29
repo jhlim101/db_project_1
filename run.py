a3dcddba060d0634c575f02fe8d0748fe848db29
Ji Han Lim <jhlim101@snu.ac.kr>
2024-03-30 01:05:54 +0900

    feat: added print statements and rule definitions for DROP TABLE, EXPLAIN, DESCRIBE, DESC, and SHOW TABLES

--- run.py
+++ run.py
@@ -2,13 +2,13 @@
 # select ID from students;
 # create table test(id int, primary key(id));
 
-from lark import Lark, Visitor
+from lark import Lark, Visitor, Transformer, UnexpectedToken
 
 PROMPT = 'DB_2022-13616>'
 
 with open('grammar.lark') as file:
     sql_parser = Lark(file.read(), start='command', lexer='basic')
-
+    
 class ReturnQueryTypeVisitor(Visitor):
     def select_query(self, tree):
         assert tree.data == 'select_query'
@@ -22,10 +22,34 @@ class ReturnQueryTypeVisitor(Visitor):
         assert tree.data == 'delete_query'
         print(PROMPT + '\'DELETE\' requested')
 
+    def describe_query(self, tree):
+        assert tree.data == 'describe_query'
+        print(PROMPT + '\'DESCRIBE\' requested')
+    
+    def desc_query(self, tree):
+        assert tree.data == 'desc_query'
+        print(PROMPT + '\'DESC\' requested')
+
+    def drop_table_query(self, tree):
+        assert tree.data == 'drop_table_query'
+        print(PROMPT + '\'DROP TABLE\' requested')
+    
+    def show_tables_query(self, tree):
+        assert tree.data == 'show_tables_query'
+        print(PROMPT + '\'SHOW TABLES\' requested')
+
+    def explain_query(self, tree):
+        assert tree.data == 'explain_query'
+        print(PROMPT + '\'EXPLAIN\' requested')
+
+
 if __name__ == "__main__":
-    bool_exit = False
-    while not bool_exit:
+    while True:
         query = input(PROMPT)
-        output_tree = sql_parser.parse(query)
-        ReturnQueryTypeVisitor().visit(output_tree)
+        try:
+            output_tree = sql_parser.parse(query)
+            ReturnQueryTypeVisitor().visit(output_tree)
+        except UnexpectedToken:
+            print(PROMPT + 'Syntax Error')
+
         
