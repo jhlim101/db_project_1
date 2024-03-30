bc523597229ae5de328e9a035cafd0d0f91b9653
Ji Han Lim <jhlim101@snu.ac.kr>
2024-03-31 00:26:12 +0900

    feat: finished project lol

--- run.py
+++ run.py
@@ -1,55 +1,86 @@
-# for testing purposes:
-# select ID from students;
-# create table test(id int, primary key(id));
-
-from lark import Lark, Visitor, Transformer, UnexpectedToken
+from lark import Lark, Visitor, UnexpectedToken
 
 PROMPT = 'DB_2022-13616>'
 
 with open('grammar.lark') as file:
     sql_parser = Lark(file.read(), start='command', lexer='basic')
-    
+
+# return query type visitor
 class ReturnQueryTypeVisitor(Visitor):
+
+    exit_flag = False
+
+    def command(self, tree):
+        if tree.children[0] == 'exit':
+            self.exit_flag = True
+
     def select_query(self, tree):
-        assert tree.data == 'select_query'
         print(PROMPT + '\'SELECT\' requested')
     
     def create_table_query(self, tree):
-        assert tree.data == 'create_table_query'
         print(PROMPT + '\'CREATE TABLE\' requested')
 
     def delete_query(self, tree):
-        assert tree.data == 'delete_query'
         print(PROMPT + '\'DELETE\' requested')
 
     def describe_query(self, tree):
-        assert tree.data == 'describe_query'
         print(PROMPT + '\'DESCRIBE\' requested')
     
     def desc_query(self, tree):
-        assert tree.data == 'desc_query'
         print(PROMPT + '\'DESC\' requested')
 
     def drop_table_query(self, tree):
-        assert tree.data == 'drop_table_query'
         print(PROMPT + '\'DROP TABLE\' requested')
     
     def show_tables_query(self, tree):
-        assert tree.data == 'show_tables_query'
         print(PROMPT + '\'SHOW TABLES\' requested')
 
     def explain_query(self, tree):
-        assert tree.data == 'explain_query'
         print(PROMPT + '\'EXPLAIN\' requested')
 
+    def insert_query(self, tree):
+        print(PROMPT + '\'INSERT\' requested')
+
+    def update_query(self, tree):
+        print(PROMPT + '\'UPDATE\' requested')
+
+# parse query function
+def parse_query(query, visitor):
+    error_flag = False
+    try:
+        output_tree = sql_parser.parse(query)
+        visitor.visit(output_tree)
+    except UnexpectedToken:
+        print(PROMPT + 'Syntax Error')
+        error_flag = True
+    return error_flag
 
 if __name__ == "__main__":
-    while True:
-        query = input(PROMPT)
-        try:
-            output_tree = sql_parser.parse(query)
-            ReturnQueryTypeVisitor().visit(output_tree)
-        except UnexpectedToken:
-            print(PROMPT + 'Syntax Error')
+    # instantiate visitor class
+    visitor = ReturnQueryTypeVisitor()
 
+    # main loop to continue until exit_flag == True
+    while not visitor.exit_flag:
+        raw_input = input(PROMPT)
+        input_list = [raw_input]
+
+        # while loop to continue receiving new lines as input until semicolon is encountered
+        while not raw_input.__contains__(";"):
+            new_line = input()
+            input_list.append(new_line)
+            if new_line.__contains__(";"):
+                break
         
+        # join query sequence together into one line, then split based on semicolon separator to parse queries separately
+        query_sequence = " ".join(input_list)
+        queries = query_sequence.split(';')
+
+        # iterate through queries and parse each query
+        for query in queries:
+            query = query.strip()
+            if query:
+                parsed_query_with_error = parse_query(query + ";", visitor)
+                # break from loop if parse_query error_flag returns True
+                if parsed_query_with_error:
+                    break
+
